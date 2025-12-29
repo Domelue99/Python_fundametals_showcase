2d50798d36a5cc6cddb611dc03e9700ec6858ba5
Domelue99 <d.l.luettich@web.de>
2025-12-29 22:26:13 +0100

    Added Get, POST and Delete functions, also implemented that every Todo needs an ID (Simulation for a DB

--- todo-api/app/routes.py
+++ todo-api/app/routes.py
@@ -1,15 +1,40 @@
 from fastapi import APIRouter
 from app.schemes import ToDoCreate
+from typing import List
+from app.schemes import TodoResponse
+from fastapi import HTTPException
+
 router = APIRouter()
 
-@router.get("/health")
-def health():
-    return{"status" : "bien"}
-
-
 todos = []
+todo_id_counter = 1
 
-@router.post("/todos", status_code=201)
+@router.post("/todos", response_model=TodoResponse)
 def create_todo(todo: ToDoCreate):
-    todos.append(todo)
-    return todo
+    global todo_id_counter
+    
+    new_todo = {
+        "id": todo_id_counter,
+        "title" : todo.title,
+        "completed" : todo.completed
+    }
+    todos.append(new_todo)
+    todo_id_counter += 1
+    return new_todo
+
+@router.get("/todos/{todos_id}", response_model=TodoResponse)
+def get_todos(todo_id:int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return todo
+        
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+@router.delete("/todos/{todos_id}")
+def delete_todo(todo_id:int):
+    for index, todo in enumerate(todos):
+        if todo["id"] == todo_id:
+            todos.pop(index)
+            return {"message" : "Todo deleted"}
+    
+    raise HTTPException(status_code=404, detail="Todo not found")
--- todo-api/app/schemes.py
+++ todo-api/app/schemes.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class ToDoCreate(BaseModel):
     title : str
     completed : bool = False
+
+class TodoResponse(BaseModel):
+    id : int
+    title : str
+    completed : bool
